c9f693f8f84bbb35d33034861e00ea171170bbfc
沈家佑 <jia.you@program.com.tw>
2019-11-20 11:31:19 +0800

    25

--- src/Customize/Config/logging.php
+++ src/Customize/Config/logging.php
@@ -6,15 +6,13 @@ return [
         //自定义频道
         'default' => [
             // 日志驱动模式
-            'driver' => 'daily',
+            'driver' => 'single',
             // 日志存放路径
             'path' => storage_path('logs/api.log'),
             // 日誌周期
             'days' => 7,
             // 日誌時區
             'timezone' => 'Asia/Taipei',
-            // 日誌權限
-            'permission' => 0644
         ],
     ],
 ];
